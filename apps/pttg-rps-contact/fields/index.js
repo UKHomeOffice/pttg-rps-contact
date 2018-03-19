5cdb7df33d4cb8fc4e42c74d750b33378490f535
Stuart Craigen <s.craigen@mac.com>
2018-03-19 09:24:25 +0000

    First commit

---
+++ apps/pttg-rps-contact/fields/index.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const dateComponent = require('hof-component-date')
+
+module.exports = {
+  name: {
+    validate: 'required'
+  },
+  surname: {
+    validate: 'required'
+  },
+  existing: {
+    validate: 'required',
+    mixin: 'radio-group',
+    options: {
+      yes: {
+        label: 'Yes',
+        value: 'Y'
+      },
+      no: {
+        label: 'No',
+        value: 'N'
+      }
+    }
+  },
+  dob: dateComponent('dob', {
+    validate: ['required', 'date', 'before']
+  }),
+  'email-address': {
+    validate: ['email', 'required']
+  }
+}
---
+++ apps/pttg-rps-contact/index.js
@@ -0,0 +1,36 @@
+'use strict'
+
+module.exports = {
+  name: 'pttg-rps-contact',
+  baseUrl: '/',
+  steps: {
+    '/contact': {
+      fields: ['title', 'name', 'surname', 'telephone', 'email-address', 'existing', 'appref', 'dob', 'nationality'],
+      next: '/confirm'
+    },
+    '/confirm': {
+      behaviours: ['complete', require('hof-behaviour-summary-page')],
+      next: '/complete'
+    },
+    '/complete': {
+      template: 'confirmation'
+    }
+  }
+}
+
+/*
+Title
+Given Name*
+Family Name*
+Telephone Guidance: we may need to contact you by phone to answer your question.
+Email*
+
+Is your question about an existing application? yes/no*
+
+If yes: Preferable if these questions only appear if the above answer is “yes”.
+
+What is your application Reference Number? Guidance: what this is and where they would get it etc.
+Or
+What is your date of birth?
+What is your nationality?
+*/
